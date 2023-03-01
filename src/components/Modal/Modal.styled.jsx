668487a8e73a1dec393e3690028a249e51c3691b
333Nikita333 <nikitosfox333@gmail.com>
2023-03-01 11:28:22 +0200

    fix modal

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -2,18 +2,29 @@ import styled from '@emotion/styled';
 
 export const Overlay = styled.div`
   position: fixed;
+  padding-top: 88px;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  right: 0;
+  bottom: 0;
   display: flex;
-  justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 1200;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 
 export const ModalBox = styled.div`
-  width: calc(100vw - 48px);
-  height: calc(100vh - 24px);
+  position: relative;
+  background-color: #fff;
+  max-width: 55%;
+  border-radius: 5px;
+
+  & img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    max-width: 100%; /* устанавливаем максимальную ширину картинки в 100% от родительского контейнера */
+  max-height: 100%; /* устанавливаем максимальную высоту картинки в 100% от родительского контейнера */
+  object-fit: contain
+  }
 `;
